16465cc9dd38cb2b487e266fa6c1275096dd84c1
ryaneggz <kre8mymedia@gmail.com>
2025-02-25 23:55:33 -0600

    Appreas to work in dev mode

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,44 @@
 import path from 'path';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
+
+
+const MANIFEST: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+  devOptions: { enabled: true },
+  manifest: {
+    name: 'Threadable Chat',
+    short_name: 'Threadable Chat',
+    description: 'Threadable Chat',
+    theme_color: '#000000',
+    background_color: '#000000',
+    display: 'standalone',
+    scope: '/',
+    start_url: '.',
+    orientation: 'portrait',
+    categories: ['education', 'productivity'],
+    icons: [
+      {
+        src: '/icons/icon-192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: '/icons/icon-512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }
+    ]
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA(MANIFEST)
+  ],
   build: {
     outDir: '../backend/src/public',
     emptyOutDir: true,
