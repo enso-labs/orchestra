a464f83a7901d6027dda4113d71d10e342ac89c0
ryaneggz <kre8mymedia@gmail.com>
2025-03-13 19:10:23 -0500

    init

--- backend/src/tools/search.py
+++ backend/src/tools/search.py
@@ -5,13 +5,13 @@ from langchain_community.utilities import SearxSearchWrapper
 @tool
 def search_engine(
     query: str,  # The search query.
-    num_results: int = 5,  # The number of results to return. Defaults to 5.
+    num_results: int = 10,  # The number of results to return. Defaults to 5.
     engines: list = [],  # The list of search engines to use. Defaults to None.
     categories: list = [],  # The list of search categories to use. Defaults to None.
     language: str = "en",  # The language to use for the search. Defaults to None.
 ) -> list:  # The search results.
     """
-    Searches for results using the SearxSearchWrapper.
+    Rewrite the users original query to be more specific and concise.
 
     Args:
         query (str): The search query.
