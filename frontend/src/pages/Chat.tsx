f09fa5165397d0bf4cb917311092d268001a22aa
ryaneggz <kre8mymedia@gmail.com>
2025-04-30 21:05:56 -0500

    Lets merge this

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,28 +1,14 @@
 import ChatLayout from "../layouts/ChatLayout"
 import { useChatContext } from "../context/ChatContext"
-import MarkdownCard from "../components/cards/MarkdownCard"
-import { Button } from "@/components/ui/button"
 import { ThreadHistoryDrawer } from "@/components/drawers/ThreadHistoryDrawer"
 import { useEffect, useRef, useState } from "react"
 import { ChatNav } from "@/components/nav/ChatNav"
-import SystemMessageCard from "@/components/cards/SystemMessageCard"
 import { ChatDrawer } from "@/components/drawers/ChatDrawer"
-import { Wrench } from "lucide-react"
-import { cn } from "@/lib/utils"
 import ChatInput from "@/components/inputs/ChatInput"
 import DefaultTool from "@/components/tools/Default"
 import SearchEngineTool from "@/components/tools/SearchEngine"
 import { findToolCall } from "@/lib/utils/format"
-
-
-interface ChatMessage {
-  role: string
-  content: string
-  type: string
-  name?: string
-  status?: string
-  tool_calls?: any[]
-}
+import ChatMessages from "@/components/lists/ChatMessages"
 
 function ToolAction({ selectedToolMessage }: { selectedToolMessage: any}) {
   if (selectedToolMessage) return (
@@ -100,10 +86,10 @@ export default function Chat() {
     <ChatLayout>
       <div
         className={`
-                flex min-h-[calc(100vh-0px)] max-h-[calc(100vh-0px)] relative
-                transition-all duration-200 ease-in-out
-                ${isAssistantOpen ? "pr-[var(--chat-drawer-width,320px)]" : ""}
-            `}
+          flex min-h-[calc(100vh-0px)] max-h-[calc(100vh-0px)] relative
+          transition-all duration-200 ease-in-out
+          ${isAssistantOpen ? "pr-[var(--chat-drawer-width,320px)]" : ""}
+      `}
       >
         <ThreadHistoryDrawer isOpen={isDrawerOpen} onClose={() => setIsDrawerOpen(false)} />
 
@@ -114,7 +100,9 @@ export default function Chat() {
           />
           <div className="flex-1 overflow-y-auto p-3 min-h-0">
             <div className="space-y-4 max-w-4xl mx-auto pb-4">
-              {!messages.find((message: ChatMessage) => message.type === "system") && currentModel?.metadata?.system_message && (
+              <ChatMessages messages={messages} />
+
+              {/* {!messages.find((message: ChatMessage) => message.type === "system") && currentModel?.metadata?.system_message && (
                 <SystemMessageCard content={payload.system} />
               )}
               {messages?.map((message: any, index: number) => {
@@ -172,7 +160,7 @@ export default function Chat() {
                     </div>
                   )
                 }
-              })}
+              })} */}
               <div ref={messagesEndRef} /> {/* Invisible element to scroll to */}
             </div>
           </div>
