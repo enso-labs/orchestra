7e701ffe50f299510d24dbf1dace17839724d24a
ryaneggz <kre8mymedia@gmail.com>
2025-05-06 18:14:55 -0500

    These scripts are useful at present

--- backend/src/tools/api.py
+++ backend/src/tools/api.py
@@ -1,8 +1,11 @@
 from typing import Union, Dict, Any, List
+import re
 import httpx
 import yaml
 from langchain_community.agent_toolkits.openapi.toolkit import RequestsToolkit
 from langchain_community.utilities.requests import TextRequestsWrapper
+from langchain_core.tools import StructuredTool
+from langchain_community.tools.requests.tool import BaseRequestsTool
 
 def _get_schema(response_json: Union[dict, list]) -> dict:
     if isinstance(response_json, list):
@@ -55,7 +58,16 @@ def get_api_spec(
         _add_endpoint_to_spec(base_url, endpoint, common_query_parameters, openapi_spec)
     return yaml.dump(openapi_spec, sort_keys=False)
 
-
+def openapi_from_url(url: str) -> str:
+    try:
+        response = httpx.get(url)
+        if response.status_code == 200:
+            return yaml.dump(response.json(), sort_keys=False)
+        else:
+            raise Exception(f"Failed to get OpenAPI spec from {url}")
+    except Exception as e:
+        raise Exception(f"Failed to get OpenAPI spec from {url}: {e}")
+    
 ################################################################################
 ### Example Usage
 ################################################################################
@@ -77,7 +89,66 @@ Here is documentation on the API:
 {api_spec}
 """.format(api_spec=api_spec)
 
+from langchain_community.utilities.requests import GenericRequestsWrapper
+from langchain_core.tools import BaseTool
+from langchain_community.tools.requests.tool import (
+    RequestsGetTool,
+    RequestsPostTool,
+    RequestsPutTool,
+    RequestsDeleteTool,
+)
+
+def get_base_tool(
+    headers: Dict[str, Any]
+) -> BaseTool:
+    requests_wrapper = GenericRequestsWrapper(headers=headers)
+    base_tool = BaseRequestsTool(requests_wrapper=requests_wrapper)
+    return base_tool
+
+def get_wrapper(
+    headers: Dict[str, Any]
+) -> GenericRequestsWrapper:
+    requests_wrapper = GenericRequestsWrapper(headers=headers)
+    return requests_wrapper
+
+def construct_api_tool(
+    name: str,
+    description: str,
+    method: str,
+    headers: Dict[str, Any],
+    metadata: Dict[str, Any] = {},
+    tags: List[str] = [],
+    verbose: bool = False,
+) -> StructuredTool:
+    requests_wrapper = GenericRequestsWrapper(headers=headers)
+    if method == "GET":
+        async def api_request(url: str):    
+            return await requests_wrapper.aget(url)
+    elif method == "POST":
+        async def api_request(url: str, data: Dict[str, Any]):    
+            return await requests_wrapper.apost(url, data=data)
+    elif method == "PUT":
+        async def api_request(url: str, data: Dict[str, Any]):    
+            return await requests_wrapper.aput(url, data=data)
+    elif method == "DELETE":
+        async def api_request(url: str):    
+            return await requests_wrapper.adelete(url)
+        
+    api_request.__doc__ = description
+    tool = StructuredTool.from_function(coroutine=api_request)
+    tool.name = re.sub(r'[^a-zA-Z0-9]', '_', name.lower())
+    tool.description = description
+    tool.metadata = metadata
+    tool.tags = tags
+    tool.verbose = verbose
+    return tool
+
 if __name__ == "__main__":
-    # print(api_spec)
-    print(system_message)
-    # print(tools)
+    tool = construct_api_tool(
+        name="Get a post",
+        description="Get a post",
+        method="GET",
+        headers={},
+        verbose=True
+    )
+    print(tool)
