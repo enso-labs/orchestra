a02ee576ff149c154bbbb0e9822947249adb84ed
Ryan Eggleston <kre8mymedia@gmail.com>
2025-06-06 20:40:50 -0500

    FROM feat/325-lighter-color-option TO development (#326)
    * init
    
    * Can bolt on a langconnect server
    
    * Update backend/src/routes/v0/__init__.py
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * API's should match for most part
    
    * init
    
    * Page in place but needs theme
    
    * Adds header
    
    * Can sselect
    
    * modularize
    
    * Page is at fix height atm
    
    * Pause here for now, we can't make auth work from UI wihtout altering Auth in app
    
    * Leave this here, redoing auth
    
    * Most of auth works, need to get it working with threads after they're created
    
    * Revert "Most of auth works, need to get it working with threads after they're created"
    
    This reverts commit 55b7a5e1b6bcd99c0768cfd59b08cae7a2b40562.
    
    * Wired auth into langconnect
    
    * API works
    
    * Can fetch the collections to sidebar
    
    * Collections routing
    
    * Need better view docs and delete docs
    
    * Can create collections and add docs to them
    
    * Table information is displayed better
    
    * Fixes logged in chat
    
    * Hotkeys work about to add react-voice-visualizer
    
    * Start from here
    
    * FROM feat/322-add-react-voice-viz INTO feat/321-knowledge-source-page (#324)
    
    * init (from 321)
    
    * has voice viz
    
    * Update where select model
    
    * small tweak
    
    * Can do rag from api but cannot do from interface
    
    * clenaup
    
    * Removed ghost folder
    
    * last fix
    
    * init
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- frontend/src/components/buttons/ColorModeButton.tsx
+++ frontend/src/components/buttons/ColorModeButton.tsx
@@ -1,4 +1,4 @@
-import { Moon, Sun } from "lucide-react"
+import { Moon, Sun, Circle } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 import {
@@ -16,14 +16,18 @@ export function ColorModeButton() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 gray:-rotate-90 gray:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 gray:rotate-90 gray:scale-0" />
+          <Circle className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-90 dark:scale-0 gray:rotate-0 gray:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
           Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("gray")}>
+          Gray
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
           Dark
