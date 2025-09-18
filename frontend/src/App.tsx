56c67c371215c951dc424d607d4083bec84571c0
ryaneggz <kre8mymedia@gmail.com>
2025-09-17 22:37:52 -0600

    fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { QueryParamProvider } from "use-query-params";
 import { ReactRouter6Adapter } from "use-query-params/adapters/react-router-6";
-import { useAppContext } from "./context/AppContext";
+// import { useAppContext } from "./context/AppContext";
 
 export default function App() {
 	// const { useDynamicScriptInjectEffect } = useAppContext();
