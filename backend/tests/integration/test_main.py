c5565b405af080d1a9a8fc7e1bd322ce56e9a78d
ryaneggz <kre8mymedia@gmail.com>
2025-10-06 19:04:22 -0600

    fix url for basic test

--- backend/tests/integration/test_main.py
+++ backend/tests/integration/test_main.py
@@ -2,6 +2,6 @@ from tests import client
 
 
 def test_ping():
-    response = client.get("/api/info")
+    response = client.get("/api/health")
     # print(response.json())
     assert response.status_code == 200
