0e8ea0963e736b71d2acd5edd2ebac12c096a5d5
ryaneggz <kre8mymedia@gmail.com>
2025-11-04 21:57:38 -0700

    Adds gridsite tool

--- backend/src/tools/__init__.py
+++ backend/src/tools/__init__.py
@@ -1,14 +1,18 @@
-from src.constants import APP_ENV
+from src.constants import APP_ENV, MICROSOFT_TEAMS_WEBHOOK_URL
 from src.tools.search import SEARCH_TOOLS
 from src.tools.test import TEST_TOOLS
 from src.tools.code import PYTHON_CODE_INTERPRETER_TOOLS
 from src.tools.finance import FINANCE_TOOLS
+from src.tools.gridsite import GRIDSIDE_TOOLS
 
 TOOL_LIBRARY = [
     *SEARCH_TOOLS,
     *PYTHON_CODE_INTERPRETER_TOOLS,
     *FINANCE_TOOLS,
 ]
+
+if MICROSOFT_TEAMS_WEBHOOK_URL:
+    TOOL_LIBRARY.extend(GRIDSIDE_TOOLS)
 
 if APP_ENV == "test":
     TOOL_LIBRARY.extend(TEST_TOOLS)
