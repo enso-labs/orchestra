96885f14b4c4aec93392051dfc6eb7f9b1fa4ab6
ryaneggz <kre8mymedia@gmail.com>
2026-02-15 20:07:00 -0700

    Select field is in place

--- frontend/src/components/lists/SelectModel.tsx
+++ frontend/src/components/lists/SelectModel.tsx
@@ -125,10 +125,10 @@ function SelectModel({
 					<ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
 				</Button>
 			</PopoverTrigger>
-			<PopoverContent className="w-[300px] p-0">
+			<PopoverContent className="w-[--radix-popover-trigger-width] p-0">
 				<Command>
 					<CommandInput placeholder="Search models..." />
-					<CommandList>
+					<CommandList className="max-h-[300px] overflow-y-auto">
 						<CommandEmpty>No model found.</CommandEmpty>
 						<CommandGroup>
 							{models.models
